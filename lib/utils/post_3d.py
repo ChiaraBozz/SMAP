c8e572fddc55f195927823104d3df65899d08d8f
ChiaraBozz <104632380+ChiaraBozz@users.noreply.github.com>
2024-04-08 12:45:17 +0200

    Update post_3d.py

--- lib/utils/post_3d.py
+++ lib/utils/post_3d.py
@@ -9,7 +9,7 @@ def back_projection(x, d, K):
     :param K: camera intrinsics
     :return: X (2xN), points in 3D
     """
-    X = np.zeros((len(d), 3), np.float)
+    X = np.zeros((len(d), 3), float)
     X[:, 0] = (x[:, 0] - K[0, 2]) * d / K[0, 0]
     X[:, 1] = (x[:, 1] - K[1, 2]) * d / K[1, 1]
     X[:, 2] = d
@@ -17,7 +17,7 @@ def back_projection(x, d, K):
 
 
 def get_3d_points(pred_bodys, root_depth, K, root_n=2):
-    bodys_3d = np.zeros(pred_bodys.shape, np.float)
+    bodys_3d = np.zeros(pred_bodys.shape, float)
     bodys_3d[:, :, 3] = pred_bodys[:, :, 3]
     for i in range(len(pred_bodys)):
         if pred_bodys[i][root_n][3] == 0:
